ed5406b6da29b32aa00b15b212a8764357da87ce
Timo Caktu <timocaktu@gmail.com>
2022-04-28 22:51:45 +0200

    sadfasdfasdf

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,16 +11,15 @@
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="/Impressum">Link</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
               <li><a class="dropdown-item" href="#">Action</a></li>
               <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
               <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li>
@@ -29,8 +28,8 @@
           </li>
         </ul>
         <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" v-model="searcher">
+          <button @click="search()" class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
     </div>
@@ -42,8 +41,13 @@ export default {
   name: "HeaderVue",
   data() {
     return {
-
+      searcher: ""
     }
+  },
+  methods: {
+    search() {
+      this.$router.push("/Impressum");
+    } 
   }
 }
 </script>
